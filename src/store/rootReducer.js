773043243c928cd94ea341b9551fbf65852ecd1c
Ben Borla <ben.borla@ballys.com>
2023-07-25 10:41:13 +0800

    cleanup

--- src/store/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import todosReducer from './todos/reducer'
-import filterReducer from './filters/reducer'
-
-export default combineReducers({
-  todos: todosReducer,
-  filter: filterReducer
-})
